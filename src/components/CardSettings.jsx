689a62b1c2c3b74937d07466817cf61aff234f29
小小梦工场 <1113638674@qq.com>
2026-04-01 17:46:39 +0800

    fix: import Button component in CardSettings

--- src/components/CardSettings.jsx
+++ src/components/CardSettings.jsx
@@ -3,6 +3,7 @@ import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Slider } from '@/components/ui/slider'
+import { Button } from '@/components/ui/button'
 
 import { FontList } from '@/config/config'
 
